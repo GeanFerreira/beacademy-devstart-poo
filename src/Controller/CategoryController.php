5ae69b20a7a723d89ca206a7b94f997fc8cc15ad
Gean Ferreira <ferreiragean@outlook.com>
2022-06-10 15:54:52 -0300

    Controller MVC com PHP puro

---
+++ src/Controller/CategoryController.php
@@ -0,0 +1,25 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controller;
+
+use App\Controller\AbstractController;
+
+class CategoryController
+{
+    public function listAction(): void
+    {
+        
+    }
+
+    public function addAction(): void
+    {
+
+    }
+
+    public function editAction(): void
+    {
+        
+    }
+}
